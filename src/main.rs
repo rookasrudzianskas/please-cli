5c49ce57f4000f84e64f2d177eac1c4da98b3cdb
Rokas <rookas.rudzenskas@gmail.com>
2022-12-27 10:19:52 +0200

    Add a new feature
    This is a new feature that does something.
    
    It is a good feature.

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use bat::PrettyPrinter;
+use clap::Parser;
+use colored::Colorize;
+use config::Config;
+use question::{Answer, Question};
+use reqwest::blocking::Client;
+use serde_json::json;
+use spinners::{Spinner, Spinners};
+use std::process::Command;
+
+mod config;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Description of the command to execute
+    prompt: Vec<String>,
+
+    /// Run the generated program without asking for confirmation
+    #[clap(short = 'y', long)]
+    force: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let config = Config::new();
+
+    let client = Client::new();
+    let mut spinner = Spinner::new(Spinners::BouncingBar, "Generating your command...".into());
+
+    let os_hint = if cfg!(target_os = "macos") {
+        " (on macOS)"
+    } else if cfg!(target_os = "linux") {
+        " (on Linux)"
+    } else {
+        ""
+    };
+
+    let response = client
+        .post("https://api.openai.com/v1/completions")
+        .json(&json!({
+            "top_p": 1,
+            "stop": "```",
+            "temperature": 0,
+            "suffix": "\n```",
+            "max_tokens": 1000,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+            "model": "text-davinci-003",
+            "prompt": format!("{}{}:\n```bash\n#!/bin/bash\n", cli.prompt.join(" "), os_hint),
+        }))
+        .header("Authorization", format!("Bearer {}", config.api_key))
+        .send()
+        .unwrap()
+        .error_for_status()
+        .unwrap_or_else(|_| {
+            spinner.stop_and_persist(
+                "✖".red().to_string().as_str(),
+                "Failed to get a response. Have you set the OPENAI_API_KEY variable?"
+                    .red()
+                    .to_string(),
+            );
+            std::process::exit(1);
+        });
+
+    let code = response.json::<serde_json::Value>().unwrap()["choices"][0]["text"]
+        .as_str()
+        .unwrap()
+        .trim()
+        .to_string();
+
+    spinner.stop_and_persist(
+        "✔".green().to_string().as_str(),
+        "Got some code!".green().to_string(),
+    );
+
+    PrettyPrinter::new()
+        .input_from_bytes(code.as_bytes())
+        .language("bash")
+        .grid(true)
+        .print()
+        .unwrap();
+
+    let should_run = if cli.force {
+        true
+    } else {
+        Question::new(
+            ">> Run the generated program? [Y/n]"
+                .bright_black()
+                .to_string()
+                .as_str(),
+        )
+        .yes_no()
+        .until_acceptable()
+        .default(Answer::YES)
+        .ask()
+        .expect("Couldn't ask question.")
+            == Answer::YES
+    };
+
+    if should_run {
+        spinner = Spinner::new(Spinners::BouncingBar, "Executing...".into());
+
+        // run command and print output and error
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(code.as_str())
+            .output()
+            .unwrap_or_else(|_| {
+                spinner.stop_and_persist(
+                    "✖".red().to_string().as_str(),
+                    "Failed to execute the generated program.".red().to_string(),
+                );
+                std::process::exit(1);
+            });
+
+        if !output.status.success() {
+            spinner.stop_and_persist(
+                "✖".red().to_string().as_str(),
+                "The program threw an error.".red().to_string(),
+            );
+            println!("{}", String::from_utf8_lossy(&output.stderr));
+            std::process::exit(1);
+        }
+
+        spinner.stop_and_persist(
+            "✔".green().to_string().as_str(),
+            "Command ran successfully".green().to_string(),
+        );
+
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+
+        config.write_to_history(code.as_str());
+    }
+}
